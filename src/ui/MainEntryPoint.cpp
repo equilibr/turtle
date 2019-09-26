2367852e21fd41b235a7448dd23d21db96ddeb3e
Sergey Tomilin <47003432+equilibr@users.noreply.github.com>
2019-09-26 10:18:00 +0300

    Moved "QCoreApplication::setAttribute(Qt::AA_UseDesktopOpenGL);" to the correct place.

--- src/ui/MainEntryPoint.cpp
+++ src/ui/MainEntryPoint.cpp
@@ -3,15 +3,15 @@
 
 int main(int argc, char** argv)
 {
-	QApplication qapp(argc, argv);
-
 #	ifdef _WIN32
-	//This MUST be executed before the main window is created, otherwise the software OpenGL
+	//This MUST be executed before QCoreApplication is created, otherwise the software OpenGL
 	//	rendered might be chosen and OSG will fail with obscure errors.
 	//For reference, see:
 	//	https://doc.qt.io/qt-5/windows-requirements.html#dynamically-loading-graphics-drivers
 	QCoreApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
 #	endif
+
+	QApplication qapp(argc, argv);
 
 	MainWindow w;
 	w.showMaximized();
