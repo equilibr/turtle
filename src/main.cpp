fd82ace18a9a4698222134fa34860c16cc92ea45
Sergey Tomilin <47003432+equilibr@users.noreply.github.com>
2019-09-29 08:16:21 +0300

    Modified the main user function to allow for selecting different algorithms.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,38 @@
 #include "main.h"
 
+#include <functional>
+#include <vector>
+#include <utility>
+
 void MainBrain(ThreadedBrain & brain)
 {
-//	MainBrainDraw(brain);
-	MainBrainFollow(brain);
+	using Algorithm = std::function<void(ThreadedBrain&)>;
+
+	std::vector<std::pair<Algorithm, QString>> algorithms =
+	{
+		{MainBrainDraw, "Draw"},
+		{MainBrainFollow, "Follow"},
+	};
+
+	QString algorithmList;
+	for (size_t i = 0; i < algorithms.size(); ++i)
+	{
+		algorithmList.append(QString::number(i) + ": ");
+		algorithmList.append(algorithms[i].second);
+		algorithmList.append("\n");
+	}
+
+	bool ok;
+	size_t algorithm = static_cast<size_t>(brain.getInteger("Select an algorithm", algorithmList, 0, &ok));
+	if (!ok)
+		return;
+
+	if (algorithm >= algorithms.size())
+	{
+		brain.log("<b>Invalid selection!</b>");
+		return;
+	}
+
+	brain.log("Executing algorithm: <b>" + algorithms[algorithm].second + "</b>");
+	algorithms[algorithm].first(brain);
 }
